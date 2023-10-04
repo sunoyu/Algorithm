609d9efe2b2630dd03c4ba3f1783e674c3a43df7
harol <harol@suno-win>
2023-10-04 23:41:13 +0900

    231004 bs

--- algorithms/src/searching/b10816_숫자카드2_BS.java
+++ algorithms/src/searching/b10816_숫자카드2_BS.java
@@ -22,10 +22,12 @@ public class b10816_숫자카드2_BS {
 		
 		m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < m; i++) {
 			int find = Integer.parseInt(st.nextToken());
-			System.out.println(upperBound(find)-lowerBound(find));
+			sb.append(upperBound(find)-lowerBound(find)).append(' ');
 		}
+		System.out.println(sb);
 		
 	}
 	
@@ -33,27 +35,35 @@ public class b10816_숫자카드2_BS {
 	// 가장 오른쪽 인덱스와 가장 왼쪽 인덱스를 찾고 두 값의 차를 갯수로 보면 된다.
 	static int upperBound(int key) {
 		int lo = 0;
-		int hi = arr.length -1;
+		int hi = arr.length;     // int[1] 의 경우 lo와 hi값이 동일하여 BS가 불가능하다. 따라서 length-1이 아닌 length로 설정!
 		
 		while(lo < hi) {
 			int mid = (hi + lo) / 2;
 
-			if(key <= arr[mid]) {
+			if(key >= arr[mid]) {
+				lo = mid + 1;
+			}
+			else hi = mid;
+			
+			/* 아래 함수와 통일성을 주고 싶다면 아래 코드로 고쳐도 되겠지.
+			if(key < arr[mid]) {
 				hi = mid;
 			}
-			else lo = mid +1;
+			else lo = mid + 1;
+			*/
+		
 		}
 		return lo;
 		
 	}
 	static int lowerBound(int key) {
 		int lo = 0;
-		int hi = arr.length -1;
+		int hi = arr.length;
 		
 		while(lo < hi) {
 			int mid = (hi + lo) / 2;
 
-			if(key < arr[mid]) {
+			if(key <= arr[mid]) {
 				hi = mid;
 			}
 			else lo = mid +1;
