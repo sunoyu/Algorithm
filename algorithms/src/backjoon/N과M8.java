bd8f676fa00613f6dfa02930683661e814b9b716
Yoo <yooseonho@yuseonhoui-MacBookPro.local>
2023-10-31 22:21:16 +0900

    테스트

---
+++ algorithms/src/backjoon/N과M8.java
@@ -0,0 +1,13 @@
+package algorithms.src.backjoon;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class N과M8 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
