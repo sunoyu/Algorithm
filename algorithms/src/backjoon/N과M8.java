c31d2bcf5e3c1a9bda389401ab598f38ad4ba251
sunoyu <suno_yu@naver.com>
2023-10-31 22:33:24 +0900

    테스트

--- algorithms/src/backjoon/N과M8.java
+++ algorithms/src/backjoon/N과M8.java
@@ -3,11 +3,24 @@ package algorithms.src.backjoon;
 import jdk.internal.util.xml.impl.Input;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N과M8 {
-    public static void main(String[] args) {
+    static int n,m;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < n ; i++){
+
+        }
+
+
     }
 }
