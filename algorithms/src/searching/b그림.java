fa9b88b8244f07655818a58110605963f7c3039b
sunoyu <suno_yu@naver.com>
2023-12-04 22:56:17 +0900

    그림

--- algorithms/src/searching/b그림.java
+++ algorithms/src/searching/b그림.java
@@ -9,8 +9,7 @@ import java.util.StringTokenizer;
 
 public class b그림 {
 
-	private static int N;
-	private static int M;
+	private static int N, M, cnt, sum;
 	private static int[][] arr;
 	private static boolean[][] visited;
 	static int max = Integer.MIN_VALUE;
@@ -36,14 +35,43 @@ public class b그림 {
 			}
 		}
 		
-		bfs();
-		
+		// arr배열을 순회하면서 1이면서 방문한적 없는 원소만 체크
+		for(int i = 0; i < N; i++) {
+			for(int j = 0 ; j < M; j++) {
+				if(arr[i][j]==1 && !visited[i][j]) {
+					cnt ++;
+					sum = 1;
+					visited[i][j] = true;
+					bfs(i,j);
+					max = Math.max(max, sum);
+				}
+			}
+		}
+		max = max == Integer.MIN_VALUE? 0 : max; // 그림이 아무것도 없는 경우 MIN_VALUE가 나와서 틀렸었음.
+		System.out.println(cnt);
+		System.out.println(max);
 	}
 
 
-	private static void bfs() {
+	private static void bfs(int sty, int stx) {
 		Queue<Nodd> q = new LinkedList<>();
-		q.add(new Nodd(0,0));
+		q.add(new Nodd(sty,stx));
+		
+		while(!q.isEmpty()) {
+			Nodd node = q.poll();
+			for(int i = 0; i <4; i++) {
+				int ny = node.y + dy[i];
+				int nx = node.x + dx[i];
+				
+				if(ny < 0 || ny >= N || nx <0 || nx >=M) continue;
+
+				if(arr[ny][nx] == 0) continue;
+				if(visited[ny][nx]) continue;
+				visited[ny][nx] = true;
+				sum ++;
+				q.add(new Nodd(ny, nx));
+			}
+		}
 		
 	}
 
