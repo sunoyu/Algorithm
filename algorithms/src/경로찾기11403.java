6c82e1f061d669ffb5892c426b9f055f80f40faa
sunoyu <suno_yu@naver.com>
2023-12-06 22:46:19 +0900

    플로이드 워셜 _ 경로찾기

---
+++ algorithms/src/경로찾기11403.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 경로찾기11403 {
+
+	private static int N;
+	private static int[][] arr;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for(int i = 0; i < N; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(arr[i][k]+arr[k][j] > 1) arr[i][j] = 1;
+				}
+			}
+		}
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+
+}
