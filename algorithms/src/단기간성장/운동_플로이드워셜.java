c4c004dba43d93d0371edc82daa8db41a2065ae6
sunoyu <suno_yu@naver.com>
2024-08-09 06:50:45 +0900

    운동_플로이드워셜. - i==j가 무한이 아닌 0으로 해서 풀었다. - 이유는 시작점에서 끝으로 돌아오는 경우의 최솟값을 구하려고

---
+++ algorithms/src/단기간성장/운동_플로이드워셜.java
@@ -0,0 +1,47 @@
+package algorithms.src.단기간성장;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 운동_플로이드워셜 {
+    private static int[][] floydMap;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        floydMap = new int[V][V];
+        int INF = 100000000;
+        // 초기화 1
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if(i==j) floydMap[i][j] = INF;
+                else floydMap[i][j] = INF;  // 무한대로 세팅
+            }
+        }
+        // 초기화 2
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            floydMap[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 플로이드 워셜
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    floydMap[i][j] = Math.min(floydMap[i][j], floydMap[i][k] + floydMap[k][j]);
+                }
+            }
+        }
+        int min = INF;
+        for (int i = 0; i < V; i++) {
+            min = min < floydMap[i][i] ? min : floydMap[i][i];
+        }
+        if(min == INF) System.out.println(-1);
+        else System.out.println(min);
+    }
+}
